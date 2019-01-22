911dde0437b8732ea44aa4f6830f21b1e8f322a9
Bin <bixu@microsoft.com>
2019-01-22 20:53:12 +0800

    only copy magnet links

--- content.js
+++ content.js
@@ -1,31 +1,31 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 	var data = '';
-	if(request == 'getAllLinks') {
+	if (request == 'getAllLinks') {
 		var theLinks = document.links;
-		for(var i=0; i<theLinks.length; i++) {
-         if(theLinks[i].href.indexOf('javascript:') == -1) data += theLinks[i].href + '\n';
+		for (var i = 0; i < theLinks.length; i++) {
+			if (theLinks[i].href.indexOf('magnet:') != -1) data += theLinks[i].href + '\n';
 		}
-	} else if(request == 'getTextLinks') {
+	} else if (request == 'getTextLinks') {
 		var theText = document.body.textContent.toString();
-		var theMatch = theText.match(/(http[^\s]*)/g);
-		for(var i=0; i<theMatch.length; i++) {
+		var theMatch = theText.match(/(magnet[^\s]*)/g);
+		for (var i = 0; i < theMatch.length; i++) {
 			data += theMatch[i] + '\n';
 		}
 	} else if (request == 'getSelectionLinks') {
-	    var theLinks = document.links;
-	    for (var i = 0; i < theLinks.length; i++) {
-	        if (document.getSelection().containsNode(theLinks[i], true) && theLinks[i].href.indexOf('javascript:') == -1) {
-	            data += theLinks[i].href + '\n';
-	        }
-	    }
-	} else if(request == 'getTextLinksSelection') {
+		var theLinks = document.links;
+		for (var i = 0; i < theLinks.length; i++) {
+			if (document.getSelection().containsNode(theLinks[i], true) && theLinks[i].href.indexOf('javascript:') == -1) {
+				data += theLinks[i].href + '\n';
+			}
+		}
+	} else if (request == 'getTextLinksSelection') {
 		var theText = window.getSelection().toString();
-		var theMatch = theText.match(/(http[^\s]*)/g);
-      if(theMatch)
-         for(var i=0; i<theMatch.length; i++) {
-            data += theMatch[i] + '\n';
-         }
+		var theMatch = theText.match(/(magnet[^\s]*)/g);
+		if (theMatch)
+			for (var i = 0; i < theMatch.length; i++) {
+				data += theMatch[i] + '\n';
+			}
 	}
-   //alert(data);
+	//alert(data);
 	sendResponse(data);
 });
